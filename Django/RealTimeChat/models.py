3ed28802de962ce208430da6fcb504c3b45af3af
wished0sea <wished0sea@gmail.com>
2023-02-02 16:18:41 +0900

    set_models

--- Django/RealTimeChat/models.py
+++ Django/RealTimeChat/models.py
@@ -4,17 +4,41 @@ from django.contrib.auth.models import AbstractUser
 
 ## member Model
 class Member(AbstractUser):
+    first_name = None
+    last_name = None
+
     ID = models.CharField("ID", max_length=20, primary_key=True)
-    nickname = models.CharField("nickname", max_length=20, unique=True)
-    email = models.CharField("email", max_length=50, null=False)
+    username = models.CharField("nickname", max_length=100, unique=True)
+    email = models.CharField("email", max_length=50, null=False, unique=True)
     email_cert = models.BooleanField("email_cert", default=False)  # 이메일 인증 여부
-    denied = models.BooleanField("denied", default=False)  # 차단 여부
-    joined_date = models.DateTimeField("joined_date")
     last_login = models.DateTimeField("last_login")
+    expired = models.BooleanField("expired", default=False)
+    expire_date = models.DateTimeField("expire_date", null=True)
 
     USERNAME_FIELD = "ID"
     REQUIRED_FIELDS = []
 
     class Meta:
         managed = True
-        db_table = "member"
+        db_table = "Member"
+
+
+## room Model
+# 필드에 PK를 선언하지 않는다면 id필드가 자동적으로 생성되며 autoIncrease가 적용된다
+class Room(models.Model):
+    room_name = models.CharField("room_name", max_length=10, unique=True)
+
+    class Meta:
+        managed = True
+        db_table = "Room"
+
+
+## Chat Log
+class ChatLog(models.Model):
+    user = models.ForeignKey("Member", on_delete=models.DO_NOTHING)
+    chat_txt = models.CharField("chat_txt", max_length=256)
+    chat_time = models.DateTimeField("chat_time")
+
+    class Meta:
+        managed = True
+        db_table = "Chat_Log"
--- README.md
+++ README.md
@@ -18,18 +18,22 @@
 > ## Start Date
 >> 2023.01.31
 >
+> ## Due Date
+>> 2023.03.31 (91 Days)  
+>
 > ## Progress
->> ### Complete
+>> ### Complete  
 >>> Setting environment (2023.01.31 ~ 2023.01.31)  
->> ### In progress
->>> Database Design (2023.01.31 ~ Today)  
->> ### Not Started
->>> Web Design ( ~ )  
->>> Web Develop ( ~ )  
+>>> Database Design (2023.01.31 ~ 2023.02.02)  
+>> ### In progress  
+>>> Web Design (2023.02.02 ~ Today)  
+>>> Web Develop (2023.02.02 ~ Today)  
+>> ### Not Started  
 >>> Web Test[Local] ( ~ )  
 >>> App Design ( ~ )  
 >>> App Develop ( ~ )   
 >>> App Test[Local] ( ~ )  
+>>> Code Optimization ( ~ )  
 >>> 
 > ## Develop Environment
 >> OS : macOS Ventura 13.2  
@@ -37,4 +41,4 @@
 >> DB : Mysql 8.0.32 [Local Laptop]  
 >>
 > # Developer
->> [김학범](https://github.com/WishedSea)
+>> [김학범](https://github.com/WishedSea)  
